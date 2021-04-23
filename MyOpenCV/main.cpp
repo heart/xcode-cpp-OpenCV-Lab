a8b1b5c715b5bf7b97a98d7b6f30f8afb87e1430
Heart Narongrit <narongrit@3dsinteractive.com>
2021-04-23 13:37:58 +0700

    Initial Commit

---
+++ MyOpenCV/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MyOpenCV
+//
+//  Created by narongrit kanhanoi on 23/4/2564 BE.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
